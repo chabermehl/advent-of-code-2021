1c709029b7b4e8f27117b557a81892f767919e18
chabermehl <clp.habermehl@gmail.com>
2022-06-18 14:40:54 -0600

    Complete day 2 part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,32 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("file not found");
+    let lines = contents.lines();
+
+    let mut depth = 0;
+    let mut horizontal_position = 0;
+
+    for line in lines {
+        let split_line: Vec<&str> = line.split(' ').collect();
+
+        match split_line[..split_line.len()] {
+            ["forward", distance, ..] => {
+                let parsed_distance: i32 = distance.parse().unwrap();
+                horizontal_position = horizontal_position + parsed_distance;
+            }
+            ["up", distance, ..] => {
+                let parsed_distance: i32 = distance.parse().unwrap();
+                depth = depth - parsed_distance;
+            }
+            ["down", distance, ..] => {
+                let parsed_distance: i32 = distance.parse().unwrap();
+                depth = depth + parsed_distance;
+            }
+            _ => println!("no match"),
+        }
+    }
+
+    println!("h pos: {}, depth: {}", horizontal_position, depth);
+    println!("exact pos: {}", horizontal_position * depth);
+}
